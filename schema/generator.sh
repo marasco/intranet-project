0ab5bc0a8207dd30da1dad14a6c408109bc45114
Francisco Marasco quiroga <francisco.marasco@gmail.com>
2017-07-29 20:22:58 -0300

    Add schemas

---
+++ schema/generator.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+php artisan infyom:scaffold User --fieldsFile=schema/User.json
+php artisan infyom:scaffold Client --fieldsFile=schema/Client.json
+php artisan infyom:scaffold Project --fieldsFile=schema/Project.json
+php artisan infyom:scaffold TimeEntry --fieldsFile=schema/TimeEntry.json
